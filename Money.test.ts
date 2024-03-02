45954f34105e9ef90206dd76df2c9ddafa2c386c
Serita Sohei <sh192b@gmail.com>
2024-03-02 12:03:15 +0900

    2章 Fix: 設計を見直す

--- Money.test.ts
+++ Money.test.ts
@@ -3,7 +3,7 @@ import { Dollar } from './Dollar';
 describe('MoneyTest', () => {
   test('掛け算のテスト', () => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toBe(10);
+    let product = five.times(2);
+    expect(product.amount).toBe(10);
   });
 });
