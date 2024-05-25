895aa36e523df8ba784a3743e92b4561f1acf9d1
Serita Sohei <sh192b@gmail.com>
2024-05-25 10:38:44 +0900

    8章 Update: 実装を隠す

--- Money.ts
+++ Money.ts
@@ -1,13 +1,39 @@
-export class Money {
+export abstract class Money {
   protected amount: number;
   constructor(amount: number) {
     this.amount = amount;
   }
+  abstract times(multiplier: number): Money;
   equals(object: any): boolean {
     if (object instanceof Money) {
       const money = object as Money;
       return this.amount === money.amount && this.constructor === money.constructor;
     }
     return false;
+  }
+  static dollar(amount: number): Money {
+    return new Dollar(amount);
+  }
+
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
+}
+
+class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+  times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+class Franc extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
   }
 }
