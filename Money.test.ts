572cec625f96090719dea864f51a38465b10690b
Serita Sohei <sh192b@gmail.com>
2024-06-13 13:22:05 +0900

    11章 Remove: フランの掛け算のテスト

--- Money.test.ts
+++ Money.test.ts
@@ -10,11 +10,6 @@ describe('MoneyTest', () => {
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
     expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
   })
-  test('Franc の掛け算のテスト', () => {
-    const five = Money.franc(5);
-    expect(five.times(2)).toEqual(Money.franc(10));
-    expect(five.times(3)).toEqual(Money.franc(15));
-  })
  test('通貨のテスト', () => {
     expect(Money.dollar(1).currency()).toBe('USD');
     expect(Money.franc(1).currency()).toBe('CHF');
